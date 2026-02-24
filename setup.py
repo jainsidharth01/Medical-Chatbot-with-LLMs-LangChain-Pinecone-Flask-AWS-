4fe2c0654f47645053ac176d1e39460e8b86c9e4
jainsiddharth01 <siddharth20432@gmail.com>
2026-02-24 16:11:55 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name='medical_chatbot',
+    version='0.1',
+    author='Siddharth jain',
+    author_email="siddharth20432@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
